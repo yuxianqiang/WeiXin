2dfc3d8b370c1dff7667cf09cccae0f9f3570586
yuxianqiang <yuxianqiang@gamil.com>
2016-09-28 07:47:08 +0800

    Add blog

--- api.php
+++
@@ -1,4 +0,0 @@
-<!--支持：youku.com，ku6.com，sohu.com，qq.com，kuwo.com,M1905.com,fun.tv,mgtv.com,yinyuetai.com,acfun.tv--><?xml version="1.0" encoding="UTF-8" ?>
-<list>
-<m label="没有获得数据" color="#00ff00,#8b00ff"/></m>
-</list>
